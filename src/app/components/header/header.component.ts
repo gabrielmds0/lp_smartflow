c1be68ff7d8b9d9c4306f56f4e7ba286c4320644
gabrielmds0 <gabrielmds0306@gmail.com>
2025-02-03 15:10:40 -0300

    Ajustes de responsividade

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,42 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener, ElementRef, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
-    selector: 'app-header',
-    imports: [CommonModule, RouterLink, ],
-    templateUrl: './header.component.html',
-    styleUrl: './header.component.css'
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  isOpen = false;
+  isMobileMenuOpen = false;
+
+  @ViewChild('mobileMenu') mobileMenu!: ElementRef;
+  @ViewChild('mobileMenuButton') mobileMenuButton!: ElementRef;
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClick(event: MouseEvent) {
+    if (
+      this.mobileMenu &&
+      this.mobileMenu.nativeElement &&
+      !this.mobileMenu.nativeElement.contains(event.target) &&
+      this.mobileMenuButton &&
+      this.mobileMenuButton.nativeElement &&
+      !this.mobileMenuButton.nativeElement.contains(event.target)
+    ) {
+      this.isMobileMenuOpen = false;
+    }
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    if (window.innerWidth >= 768) {
+      this.isMobileMenuOpen = false;
+    }
+  }
 }
